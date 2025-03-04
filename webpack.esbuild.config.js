92cac237afc9eb198409722a1baf20aac7ae803a
Hưng Nguyễn <hungnguyen101196.com>
2025-03-05 01:08:12 +0700

    fix config to build with es build

--- webpack.esbuild.config.js
+++ webpack.esbuild.config.js
@@ -1,23 +1,38 @@
-const { EsbuildPlugin } = require('esbuild-loader');
+const { EsbuildPlugin } = require("esbuild-loader");
 
 module.exports = {
   module: {
     rules: [
       {
         test: /\.[jt]sx?$/,
-        loader: 'esbuild-loader',
+        loader: "esbuild-loader",
         options: {
-          loader: 'tsx',
-          target: 'es2020'
-        }
-      }
-    ]
+          loader: "tsx",
+          target: "es2020",
+        },
+      },
+    ],
   },
   optimization: {
+    splitChunks: false, // Disable code splitting
+    runtimeChunk: false, // Disable runtime chunk
+    minimize: true,
     minimizer: [
       new EsbuildPlugin({
-        target: 'es2020'
-      })
-    ]
-  }
+        target: "es2020",
+      }),
+    ],
+  },
+  experiments: {
+    outputModule: true,
+  },
+  output: {
+    library: {
+      type: "module",
+    },
+    filename: "[name].js", // Use chunk names for filenames
+    chunkFilename: "[name].js",
+    chunkFormat: "module", // Force ESM format
+    chunkLoading: false, // Disable Webpack chunk loading
+  },
 };
